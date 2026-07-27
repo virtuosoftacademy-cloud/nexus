ff8bcd20ce23ca8efa397d07b2d9ddbda7e04cd6
Virtuosoft <virtuosoftacademy@gmail.com>
2026-07-27 14:10:18 +0500

    changes

--- components/ui/safeimage.tsx
+++ components/ui/safeimage.tsx
@@ -10,6 +10,7 @@ export default function SafeImage({ src, alt, ...rest }: ImageProps) {
         <Image
             {...rest}
             alt={alt}
+            loading="eager"
             src={failed ? FALLBACK_POST_IMAGE : src}
             onError={() => setFailed(true)}
         />
